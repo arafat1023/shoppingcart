96113296ec32e5f40f27e7120183779b8722c638
arafat1023 <arafathossain847@gmail.com>
2022-11-20 12:37:01 +0600

    open close cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,11 @@
 import {Offcanvas} from 'react-bootstrap'
 import {useShoppingCart} from "../context/ShoppingCartContext";
-export function ShoppingCart(){
-    const {closeCart}=useShoppingCart({isOpen})
+
+type ShoppingCartProps = {
+    isOpen: boolean
+}
+export function ShoppingCart({isOpen}:ShoppingCartProps){
+    const {closeCart}=useShoppingCart()
     return <Offcanvas show={isOpen} onHide={closeCart} placement="end">
         <Offcanvas.Header closeButton>
             <Offcanvas.Title>Cart</Offcanvas.Title>
